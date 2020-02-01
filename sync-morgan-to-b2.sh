0b63c334ea7b68c8e681110fbf4f59e45bc2f1c8
Mark Ormesher <me@markormesher.co.uk>
2020-02-01 14:27:11 +0000

    bail on b2 sync if rclone is already running

--- sync-morgan-to-b2.sh
+++ sync-morgan-to-b2.sh
@@ -1,6 +1,18 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
+function msg() {
+  echo
+  echo "[$(date -Iseconds)] $1"
+}
+
+if pgrep rclone > /dev/null; then
+  msg "rclone is already running - aborting"
+  exit 0
+fi
+
+exit 0
+
 export TMPDIR=/borg/tmp
 
 rclone --transfers=1 --drive-chunk-size=250M "$@" sync /borg/repo0 b2-borg-repo0-crypt:
